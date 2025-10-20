621744c3ddd670fefb7bd0153027d09ee8aea449
theeasylearn <theeasylearn@gmail.com>
2025-10-20 10:27:39 +0530

    list

--- basics/7_list_2.dart
+++ basics/7_list_2.dart
@@ -1,27 +1,30 @@
-import 'dart:io';
 void main()
 {
     int size = 3;
-    int defaultValue = 0;
-    //create fixed type dynamic size list (we can insert any number of value)
-    List<int> flutter12 = new List<int>.filled(size,defaultValue,growable:true);
-    print(flutter12);
-    //store value at position
-    flutter12[0] = 100;
-    flutter12[1] = 50;
-    flutter12[2] = 70;
-    print(flutter12);
+    int value = 0;
+    //create fixed type dynamic size list
+    List<int> ids = new List.filled(size,value,growable:true);
+    print(ids); // [0,0,0]
+    //update list 
+    ids[0] = 100;
+    ids[1] = 200;
+    ids[2] = 300;
+    ids.add(400); 
+    ids.add(500); 
+    ids.add(1600);
+    print(ids); 
+    ids.remove(100); //remove by value 
+    ids.removeAt(2);
+    print(ids); 
 
-    flutter12.add(200);
-    flutter12.add(300);
-    flutter12.add(400);
-    flutter12.add(500);
-    flutter12.add(1500);
-
-    print(flutter12);
-
-    // error because list is of integer type
-    // flutter12.add('Ankit');
-    // flutter12.add(12.22);
-    // flutter12.add(false);
+    //mixed type dynamic list 
+    var items = ['Sparkels','Sweets',200,true,3.14];
+    print(items);
+    items.add('car');
+    items.insert(0,9); //insert new value at begining
+    items.insert(2,false); //insert new value at given position
+    items[items.indexOf('Sweets')] = "namkeen";
+    print(items);
+    var wishes = [];
+    print(wishes);
 }
--- basics/8_list_3.dart
+++ basics/8_list_3.dart
@@ -1,13 +1,26 @@
 void main()
 {
-    //create mixed type dynamic list 
-    var mixedList = ['the easylearn academy',2012,true,10.7];
-    print(mixedList);
+    //how to combine list 
+    var list1 = [10,20,30];
+    var list2 = [40,50];
+    var list3 = [80,90,100];
+    var list4 = null;
+    print(list1);
+    print(list2);
+    print(list3);
+    //cobine 
+    var combinedList1 = List.from(list1)..addAll(list2)..addAll(list3);
+    print(combinedList1);
 
-    //we can add any type of value 
-    mixedList.add(200);
-    mixedList.add(500.99);
-    mixedList.add('Bhavnagar');
+    var combinedList2 = [list1,list2,list3].expand((value) => value).toList();
+    print(combinedList2);
 
-    print(mixedList);   
+    var combinedList3 = list1 + list2 + list3;
+    print(combinedList3);
+
+    var combinedList4 = [...list1,...list2,...list3];
+    print(combinedList4);
+
+    var combinedList5 = [...list1,...list2,...list3,...?list4];
+    print(combinedList5);
 }
